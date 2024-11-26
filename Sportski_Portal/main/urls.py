24cb220b06c9f8d08e08fca50fc91dce2d8623df
Miske9 <124712586+Miske9@users.noreply.github.com>
2024-11-26 02:14:55 +0100

    templates

--- Sportski_Portal/main/urls.py
+++ Sportski_Portal/main/urls.py
@@ -1,8 +1,12 @@
 from django.urls import path # type: ignore
 from . import views
 
-app_name = 'main'  
+app_name='main'
 
 urlpatterns = [
-    path('', views.homepage, name='homepage'),
+    path('igraci/', views.igraci, name='igraci'),
+    path('homepage/', views.homepage, name='homepage'),
+    path('timovi/', views.timovi, name='timovi'),
+    path('utakmice/', views.utakmice, name='utakmice'),
+    path('natjecanja/', views.natjecanja, name='natjecanja'),
 ]
--- Sportski_Portal/main/views.py
+++ Sportski_Portal/main/views.py
@@ -2,5 +2,18 @@ from django.shortcuts import render # type: ignore
 from django.http import HttpResponse # type: ignore
 
 def homepage(request):
-    return HttpResponse('Welcome to homepage! <strong>#samoOIRI</strong>')
+    return render(request, 'homepage.html')
+
+def igraci(request):
+    return render(request, 'igraci.html')
+
+def timovi(request):
+    return render(request, 'timovi.html')
+
+def utakmice(request):
+    return render(request, 'utakmice.html')
+
+def natjecanja(request):
+    return render(request, 'natjecanja.html')
+
     
