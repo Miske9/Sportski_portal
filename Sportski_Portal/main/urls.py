cc416b2d4e6186e96a4817d566bd6de69e10bfc4
Miske9 <124712586+Miske9@users.noreply.github.com>
2024-11-27 11:00:42 +0100

    uklonjeni templates

--- Sportski_Portal/main/urls.py
+++ Sportski_Portal/main/urls.py
@@ -4,9 +4,5 @@ from . import views
 app_name='main'
 
 urlpatterns = [
-    path('igraci/', views.igraci, name='igraci'),
     path('homepage/', views.homepage, name='homepage'),
-    path('timovi/', views.timovi, name='timovi'),
-    path('utakmice/', views.utakmice, name='utakmice'),
-    path('natjecanja/', views.natjecanja, name='natjecanja'),
 ]
